1c133dec7acf98ae37048420c19f6a9c4ef44512
Suyash Srivastava <suyash@Suyashs-MacBook-Air.local>
2018-09-20 22:05:13 +0100

    conversion working

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const http = require('http');
+const bodyParser = require('body-parser');
 
 const config = require('./config/config.json');
 const PORT = config.port;
 
 const app = express();
+app.use(bodyParser.json());
 
-var myapi = require ('./currency-api');
+var myapi = require('./currency-api');
 
 const handler = (req, res) => {
-    console.log('test')
-    res.sendStatus(200);
+    var from = req.body.from;
+    var to = req.body.to;
+
+    myapi(from, to, (error, amount) => {
+        if (error) {
+            res.json(error.stack);
+        } else {
+            res.status(200).send({
+                "amount": amount
+            });
+        }
+
+    });
 };
 
 app.post('/convert', handler);
